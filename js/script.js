38fd4350b37e5a7fc359fd0bc43a5ac4070e7e57
lindsayagreen <lindsgree@gmail.com>
2023-09-28 10:16:59 -0500

    quick changes

--- js/script.js
+++ js/script.js
@@ -10,17 +10,16 @@
 //WHEN the game is over
 //THEN I can save my initials and my score
 
-var startButton = document.querySelector("#.start-button");
-var submitButton = document.querySelector (".submit-button");
-var saveButton = document.querySelector(".save-button");
+var startButton = document.querySelector(".start-button");
 var timerElement = document.querySelector(".timer");
 var questionContainer = document.querySelector(".question-container");
+var questionText = document.querySelector('question-text');
 var timeLeft = document.querySelector("time-left");
-
+// var submitButton = document.querySelector (".submit-button");
+var saveButton = document.querySelector(".save-button");
 
 
 // What is the difference between getElement and QuerySelector? WHy use one over the other? 
-
 
 // Const instead of var- look this up to confirm 
 var questions = [
@@ -35,19 +34,33 @@ var questions = [
 ];
 
 
-
 startButton.addEventListener("click", startQuiz);
-submitButton.addEventListener("click", submitQuiz);
 saveButton.addEventListener("click", saveScore);
 
 
+function startQuiz() {
+
+    // Display the first question (you can replace this with your question logic)
+    showQuestion();
+
+    // Start the timer
+    startTimer();
+}
+
+function startTimer() {
+    let timeLeft = 60;
+
+
+}
 
 
 
 
 
-    // Save the initials and score in some data structure or send to a server
-    // Redirect to a high score page or display the high scores
+
+
+// Save the initials and score in some data structure or send to a server
+// Redirect to a high score page or display the high scores
 // function saveScore() {
 //     const initials = initialsInput.value;
 
